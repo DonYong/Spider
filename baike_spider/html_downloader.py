6fa33554d4136b3c84c492eff207b9a402fa940e
DonYong <49349669+DonYong@users.noreply.github.com>
2020-09-13 14:56:10 +0800

    Add files via upload

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,14 @@
+import urllib
+
+
+class HtmlDownloader(object):
+
+    def downloader(self, url):
+        if url is None:
+            return None
+
+        response = urllib.urlopen(url)
+
+        if response.getcode() != 200:
+            return None
+        return response.read()
